20fe05565486678dca665fa01ac82ccbdd1c7cd8
dgutper960 <dgutper960@outlook.es>
2024-04-09 13:01:57 +0200

    itera sobre observable

--- appCoches/src/app/home/home.page.ts
+++ appCoches/src/app/home/home.page.ts
@@ -1,12 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+ selector: 'app-home',
+ templateUrl: 'home.page.html',
+ styleUrls: ['home.page.scss'],
 })
 export class HomePage {
 
-  constructor() {}
+ // Variable para almacenar el string del input filtro
+ filtro: string = '';
 
+ // Variable que almacena los datos que llegan del JSON
+ coches: any;
+
+ // Cargaremos los datos el JSON nada más abrir la aplicación
+ constructor(private httpClient: HttpClient) {
+    this.httpClient.get('https://raw.githubusercontent.com/dgutper960/HLC_1.14_appResponsiva/master/coches.json').subscribe((data: any) => {
+      this.coches = data;
+    });
+ }
 }
