2556ec63b7b914112e456bccbd433f47dd9e0923
dgutper960 <dgutper960@outlook.es>
2024-04-10 12:38:24 +0200

    añade imagen de avatar

--- appCoches/src/app/home/home.page.ts
+++ appCoches/src/app/home/home.page.ts
@@ -14,10 +14,19 @@ export class HomePage {
  // Variable que almacena los datos que llegan del JSON
  coches: any;
 
+ // Variable que va a recoger la imagen de unsplash
+ imagen: any;
+
  // Cargaremos los datos el JSON nada más abrir la aplicación
  constructor(private httpClient: HttpClient) {
     this.httpClient.get('https://raw.githubusercontent.com/dgutper960/HLC_1.14_appResponsiva/master/coches.json').subscribe((data: any) => {
       this.coches = data;
+    });
+
+     // Configuramos la solicitud para obtener la imagen como un Blob
+    this.httpClient.get('https://source.unsplash.com/random/?car', { responseType: 'blob' }).subscribe((response: Blob) => {
+      // Convertimos el Blob a un objeto URL para poder usarlo en el template
+      this.imagen = URL.createObjectURL(response);
     });
  }
 }
